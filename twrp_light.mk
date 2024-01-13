a5c896112cdc73720269a8f81a0fa967d1151b11
IMYdev <omranmansour096@gmail.com>
2024-01-13 19:24:17 +0200

    light: Initial commit Of Twrp Tree

---
+++ twrp_light.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from light device
+$(call inherit-product, device/xiaomi/light/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := light
+PRODUCT_NAME := twrp_light
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO M4 5G # Redmi 10 5G
+PRODUCT_MANUFACTURER := xiaomi
